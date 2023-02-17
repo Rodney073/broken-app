fb6fe2d9008c3fb72c6345d48ad0765a48b1bea0
Daniel Soos <extern.daniel.soos@volkswagen.de>
2023-02-17 13:21:36 +0100

    refactored StockDTO to record

--- broken-app/src/main/java/bredex/brokenapp/model/StockDTO.java
+++ broken-app/src/main/java/bredex/brokenapp/model/StockDTO.java
@@ -3,10 +3,5 @@ package bredex.brokenapp.model;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public record StockDTO(String id, String name) implements Serializable {
 }
