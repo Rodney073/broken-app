e57266e70976f3a010c90208e89b9d65f3694912
Seller, Gabor (EXTERN: Bredex GmbH) <gabor.seller@bredex.de>
2023-02-17 13:44:59 +0100

    endpoint url

--- broken-app/src/main/java/bredex/brokenapp/adapter/web/UserController.java
+++ broken-app/src/main/java/bredex/brokenapp/adapter/web/UserController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
 }
