9ce0b895dec6a83325f46a56853c62ff68419039
Daniel Soos <extern.daniel.soos@volkswagen.de>
2023-02-17 14:57:58 +0100

    added endpoint method

--- broken-app/src/main/java/bredex/brokenapp/adapter/web/ShareController.java
+++ broken-app/src/main/java/bredex/brokenapp/adapter/web/ShareController.java
@@ -1,10 +1,28 @@
 package bredex.brokenapp.adapter.web;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import bredex.brokenapp.model.ShareDTO;
+import bredex.brokenapp.services.api.ShareService;
 
 @RestController
 @RequestMapping("/share")
 public class ShareController {
+
+    @Autowired
+    private ShareService service;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{userId}")
+    public List<ShareDTO> get(
+            @PathVariable("userId")
+            Integer userId) {
+        return service.getShares(userId);
+    }
 }
