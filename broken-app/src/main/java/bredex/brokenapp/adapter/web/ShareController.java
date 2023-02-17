f74a77568c1fdce4bf6b98cbb9c1c4772f1c9725
Rodney073 <monika.ferenczi@bredex.de>
2023-02-17 11:21:05 +0100

    Add basic folder structure

---
+++ broken-app/src/main/java/bredex/brokenapp/adapter/web/ShareController.java
@@ -0,0 +1,10 @@
+package bredex.brokenapp.adapter.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ShareController {
+}
